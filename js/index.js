f45e3c3d269187f2e16c65eac53a60a517b900d7
Hendy <hendy.almeida@hotmail.com>
2020-03-28 11:26:33 -0300

    fix front

--- js/index.js
+++ js/index.js
@@ -14,6 +14,39 @@ fetch("http://localhost:8000/api/devs", {
 })
 .then(response => response.json())
 .then(data => { 
-  // recebo informação em array e posso exibir no console ou html
-  console.log(data); 
+  // recebo informação em array e posso exibir no console ou html 
+  let devs = data;
+  let html = '';
+
+  devs.forEach(dev => {
+    html += `
+    <div class="card">
+      <img src="images/dev-img1.svg" class="card-img-top">
+      <div class="card-body">
+          <h5 class="card-title">${dev.name}</h5>
+          <p class="card-text">${dev.email}</p>
+          <p class="card-text">${dev.level}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+          <h5 class="card-title text-center">Skills</h5>
+          <li class="list-group-item">HTML</li>
+          <li class="list-group-item">JavaScript</li>
+          <li class="list-group-item">Php</li>
+      </ul>
+      <div class="card-body text-center">
+          <a href="${dev.github}" class="card-link">
+              <p class="card-text">
+              Github 
+              <img src="images/logo-github.svg" class="icon-github">
+              </p>
+          </a>
+      </div>
+    </div>
+    `
+  });
+
+  let containerDevs = document.querySelector('#devs'); 
+
+  containerDevs.insertAdjacentHTML('beforeend', html);
+
 });
